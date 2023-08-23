9935de833966544ebd7a084f5c7924d8fec3490b
Siwakorn Banluesapy <siwakorn167@gmail.com>
2023-08-23 23:06:15 +0700

    new change

--- ConfigDB.php
+++ ConfigDB.php
@@ -8,11 +8,8 @@ try {
     $connSystem = new PDO(
         "mysql:host=" . $mysql['host'] . "; dbname=" . $mysql['database'] . ";charset=utf8",
         $mysql['username'],
-        $mysql['password'],
-        // [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]
-        // array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")
-    ); //กำหนดการเชื่อมต่อเป็น utf8
-
+        $mysql['password']
+    );
 } catch (PDOException $e) { //ดักจับ ERROR แล้วเก็บไว้ใน $e
     echo $e->getMessage(); # แสดงออกมาหน้าจอ
 }
