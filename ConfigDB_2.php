289753bef8df13a147ef72068712a1db67b46161
Siwakorn Banluesapy <siwakorn167@gmail.com>
2023-08-23 23:00:18 +0700

    new change

--- ConfigDB_2.php
+++ ConfigDB_2.php
@@ -15,7 +15,7 @@ class Database
 
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8mb4", $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // $this->conn->exec("set names utf8");
         } catch (PDOException $exception) {
