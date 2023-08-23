32b8c64e4cae99d6b4cdc8eed96cb8984145abd6
Siwakorn Banluesapy <siwakorn167@gmail.com>
2023-08-23 22:48:54 +0700

    new change

--- ConfigDB.php
+++ ConfigDB.php
@@ -6,11 +6,11 @@ try {
     $mysql['database'] = 'etuter'; //ชื่อฐานข้อมูล
 
     $connSystem = new PDO(
-        "mysql:host=" . $mysql['host'] . "; dbname=" . $mysql['database'] . "",
+        "mysql:host=" . $mysql['host'] . "; dbname=" . $mysql['database'] . ";charset=utf8mb4;",
         $mysql['username'],
         $mysql['password'],
         // [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]
-        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")
+        // array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")
     ); //กำหนดการเชื่อมต่อเป็น utf8
 
 } catch (PDOException $e) { //ดักจับ ERROR แล้วเก็บไว้ใน $e
